c6ba86605598429e8b2b00d6b08caca575f46a9b
Paweł Kapalla <pkapalla@xesenix.pl>
2018-05-15 01:00:25 +0200

    XG-17 test(movement): optimize tests

--- src/lib/game/ancient-maze/algorithm.spec.ts
+++ src/lib/game/ancient-maze/algorithm.spec.ts
@@ -1,3 +1,4 @@
+import { Container } from 'inversify';
 import { IAncientMazeState } from 'lib/game/ancient-maze/ancient-maze';
 import Board from 'lib/game/board/board';
 import { IGameObjectState, IMovableGameObjectState } from 'lib/game/board/interface';
@@ -9,51 +10,63 @@ import { ARROW_TYPE, BOX_TYPE, ObjectFactory, PLAYER_TYPE, ROCK_TYPE } from './o
 
 type GO = (IGameObjectState | IMovableGameObjectState);
 
-fdescribe('commands', () => {
+const resolveState = (di: Container, state: IAncientMazeState<GO>) => {
+	const algorithm: Algorithm<GO, IAncientMazeState<GO>> = di.get('game-engine');
+	algorithm.commandAction(state);
+	do {
+		state.objects.filter((obj) => obj.state.alive).forEach((obj) => {
+			state.board.remove(obj.state.position.x, obj.state.position.y, obj);
+			state.board.add(obj.state.position.x, obj.state.position.y, obj);
+		});
+		algorithm.update(state);
+	} while (!algorithm.resolved(state));
+};
+
+describe('commands', () => {
+	let di: Container;
+
+	beforeEach(() => {
+		di = new Container();
+		di.bind<CollisionSystem<GO, IAncientMazeState<GO>>>('collision-system').to(CollisionSystem).inSingletonScope();
+		di.bind<Algorithm<GO, IAncientMazeState<GO>>>('game-engine').to(Algorithm).inSingletonScope();
+		di.bind<IAncientMazeState<GO>>('state').toConstantValue({
+			objects: [],
+			inputBuffer: [],
+			finished: false,
+			command: undefined, // tested command
+			executedCommands: [],
+			collected: { },
+			initialCollectableCount: { },
+			steps: 0,
+			board: new Board(5, 5),
+		} as IAncientMazeState<GO>);
+	});
 
 	describe('up', () => {
+		beforeEach(() => {
+			const state: IAncientMazeState<GO> = di.get('state');
+			state.command = 'up';
+		});
+
 		[
 			{ posX: 0, posY: 1 },
 			{ posX: 2, posY: 3 },
 		].forEach(({ posX, posY }) => {
 			it('should move (rock, box, player) up if they are adjacent one over each other', () => {
-				const collisionSystem = new CollisionSystem<GO, IAncientMazeState<GO>>();
-				const algorithm = new Algorithm<GO, IAncientMazeState<GO>>(collisionSystem);
 				const objectFactory = new ObjectFactory();
-				const state: IAncientMazeState<GO> = {
-					objects: [],
-					inputBuffer: [],
-					finished: false,
-					command: 'up', // tested command
-					executedCommands: [],
-					collected: { },
-					initialCollectableCount: { },
-					steps: 0,
-					board: new Board(5, 5),
-				} as IAncientMazeState<GO>;
+				const state: IAncientMazeState<GO> = di.get('state');
 
 				// setup board
-				let rock: GameBoardObject<GO>;
-				let box: GameBoardObject<GO>;
-				let player: GameBoardObject<GO>;
-				rock = objectFactory.create(ROCK_TYPE, { x: posX, y: posY });
-				box = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
-				player = objectFactory.create(PLAYER_TYPE, { x: posX, y: posY + 2 });
+				const rock: GameBoardObject<GO> = objectFactory.create(ROCK_TYPE, { x: posX, y: posY });
+				const box: GameBoardObject<GO> = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
+				const player: GameBoardObject<GO> = objectFactory.create(PLAYER_TYPE, { x: posX, y: posY + 2 });
+
 				state.objects.push(rock);
 				state.objects.push(box);
 				state.objects.push(player);
 
 				// start execution
-				algorithm.commandAction(state);
-				console.log('init', state);
-				do {
-					state.objects.filter((obj) => obj.state.alive).forEach((obj) => {
-						state.board.remove(obj.state.position.x, obj.state.position.y, obj);
-						state.board.add(obj.state.position.x, obj.state.position.y, obj);
-					});
-					algorithm.update(state);
-					console.log('update', state);
-				} while (!algorithm.resolved(state));
+				resolveState(di, state);
 
 				expect(rock.state.position).toEqual({ x: posX, y: 0 }, 'should move rock');
 				expect(box.state.position).toEqual({ x: posX, y: posY }, 'should move box');
@@ -66,42 +79,18 @@ fdescribe('commands', () => {
 			{ posX: 2, posY: 3 },
 		].forEach(({ posX, posY }) => {
 			it('should move (box, arrow) if they are entering the same cell', () => {
-				const collisionSystem = new CollisionSystem<GO, IAncientMazeState<GO>>();
-				const algorithm = new Algorithm<GO, IAncientMazeState<GO>>(collisionSystem);
 				const objectFactory = new ObjectFactory();
-				const state: IAncientMazeState<GO> = {
-					objects: [],
-					inputBuffer: [],
-					finished: false,
-					command: 'up', // tested command
-					executedCommands: [],
-					collected: { },
-					initialCollectableCount: { },
-					steps: 0,
-					board: new Board(5, 5),
-				} as IAncientMazeState<GO>;
+				const state: IAncientMazeState<GO> = di.get('state');
 
 				// setup board
-				let box: GameBoardObject<GO>;
-				let arrow: GameBoardObject<GO>;
-
-				box = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
-				arrow = objectFactory.create(ARROW_TYPE, { x: posX + 1, y: posY }, { x: -1, y: 0 });
+				const box: GameBoardObject<GO> = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
+				const arrow: GameBoardObject<GO> = objectFactory.create(ARROW_TYPE, { x: posX + 1, y: posY }, { x: -1, y: 0 });
 
 				state.objects.push(box);
 				state.objects.push(arrow);
 
 				// start execution
-				algorithm.commandAction(state);
-				console.log('init', state);
-				do {
-					state.objects.filter((obj) => obj.state.alive).forEach((obj) => {
-						state.board.remove(obj.state.position.x, obj.state.position.y, obj);
-						state.board.add(obj.state.position.x, obj.state.position.y, obj);
-					});
-					algorithm.update(state);
-					console.log('update', state);
-				} while (!algorithm.resolved(state));
+				resolveState(di, state);
 
 				expect(box.state.position).toEqual({ x: posX, y: posY }, 'should move box');
 				expect(arrow.state.position).toEqual({ x: posX + 1, y: posY }, 'should move arrow');
@@ -111,48 +100,30 @@ fdescribe('commands', () => {
 	});
 
 	describe('down', () => {
+		beforeEach(() => {
+			const state: IAncientMazeState<GO> = di.get('state');
+			state.command = 'down';
+		});
+
 		[
 			{ posX: 0, posY: 1 },
 			{ posX: 2, posY: 0 },
 		].forEach(({ posX, posY }) => {
 			it('should move (rock, box, player) up if they are adjacent one over each other', () => {
-				const collisionSystem = new CollisionSystem<GO, IAncientMazeState<GO>>();
-				const algorithm = new Algorithm<GO, IAncientMazeState<GO>>(collisionSystem);
 				const objectFactory = new ObjectFactory();
-				const state: IAncientMazeState<GO> = {
-					objects: [],
-					inputBuffer: [],
-					finished: false,
-					command: 'down', // tested command
-					executedCommands: [],
-					collected: { },
-					initialCollectableCount: { },
-					steps: 0,
-					board: new Board(5, 5),
-				} as IAncientMazeState<GO>;
+				const state: IAncientMazeState<GO> = di.get('state');
 
 				// setup board
-				let rock: GameBoardObject<GO>;
-				let box: GameBoardObject<GO>;
-				let player: GameBoardObject<GO>;
-				player = objectFactory.create(PLAYER_TYPE, { x: posX, y: posY });
-				box = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
-				rock = objectFactory.create(ROCK_TYPE, { x: posX, y: posY + 2 });
+				const player: GameBoardObject<GO> = objectFactory.create(PLAYER_TYPE, { x: posX, y: posY  });
+				const box: GameBoardObject<GO> = objectFactory.create(BOX_TYPE, { x: posX, y: posY + 1 });
+				const rock: GameBoardObject<GO> = objectFactory.create(ROCK_TYPE, { x: posX, y: posY + 2 });
+
 				state.objects.push(rock);
 				state.objects.push(box);
 				state.objects.push(player);
 
 				// start execution
-				algorithm.commandAction(state);
-				console.log('init', state);
-				do {
-					state.objects.filter((obj) => obj.state.alive).forEach((obj) => {
-						state.board.remove(obj.state.position.x, obj.state.position.y, obj);
-						state.board.add(obj.state.position.x, obj.state.position.y, obj);
-					});
-					algorithm.update(state);
-					console.log('update', state);
-				} while (!algorithm.resolved(state));
+				resolveState(di, state);
 
 				expect(rock.state.position).toEqual({ x: posX, y: 4 }, 'should move rock');
 				expect(box.state.position).toEqual({ x: posX, y: posY + 2 }, 'should move box');
@@ -165,42 +136,18 @@ fdescribe('commands', () => {
 			{ posX: 2, posY: 3 },
 		].forEach(({ posX, posY }) => {
 			it('should move (box, arrow) if they are entering the same cell', () => {
-				const collisionSystem = new CollisionSystem<GO, IAncientMazeState<GO>>();
-				const algorithm = new Algorithm<GO, IAncientMazeState<GO>>(collisionSystem);
 				const objectFactory = new ObjectFactory();
-				const state: IAncientMazeState<GO> = {
-					objects: [],
-					inputBuffer: [],
-					finished: false,
-					command: 'down', // tested command
-					executedCommands: [],
-					collected: { },
-					initialCollectableCount: { },
-					steps: 0,
-					board: new Board(5, 5),
-				} as IAncientMazeState<GO>;
+				const state: IAncientMazeState<GO> = di.get('state');
 
 				// setup board
-				let box: GameBoardObject<GO>;
-				let arrow: GameBoardObject<GO>;
-
-				box = objectFactory.create(BOX_TYPE, { x: posX, y: posY - 1 });
-				arrow = objectFactory.create(ARROW_TYPE, { x: posX + 1, y: posY }, { x: -1, y: 0 });
+				const box: GameBoardObject<GO> = objectFactory.create(BOX_TYPE, { x: posX, y: posY - 1 });
+				const arrow: GameBoardObject<GO> = objectFactory.create(ARROW_TYPE, { x: posX + 1, y: posY }, { x: -1, y: 0 });
 
 				state.objects.push(box);
 				state.objects.push(arrow);
 
 				// start execution
-				algorithm.commandAction(state);
-				console.log('init', state);
-				do {
-					state.objects.filter((obj) => obj.state.alive).forEach((obj) => {
-						state.board.remove(obj.state.position.x, obj.state.position.y, obj);
-						state.board.add(obj.state.position.x, obj.state.position.y, obj);
-					});
-					algorithm.update(state);
-					console.log('update', state);
-				} while (!algorithm.resolved(state));
+				resolveState(di, state);
 
 				expect(box.state.position).toEqual({ x: posX, y: posY }, 'should move box');
 				expect(arrow.state.position).toEqual({ x: posX + 1, y: posY }, 'should move arrow');
