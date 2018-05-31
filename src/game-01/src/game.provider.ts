2bea2848b799bd0b50d0ad745085144152d3861c
Paweł Kapalla <pkapalla@xesenix.pl>
2018-06-01 00:04:40 +0200

    XG-20 fix(phaser): fix phaser typedefinitions

--- src/game-01/src/game.provider.ts
+++ src/game-01/src/game.provider.ts
@@ -4,7 +4,7 @@ import * as Phaser from 'phaser';
 
 const gameProvider = (canvas: HTMLCanvasElement): Phaser.Game => {
 	/** @see https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js */
-	const fps: Phaser.FPSConfig = {
+	const fps: FPSConfig = {
 		min: 10,
 		target: 30,
 		forceSetTimeOut: false,
@@ -12,28 +12,30 @@ const gameProvider = (canvas: HTMLCanvasElement): Phaser.Game => {
 		panicMax: 120,
 	};
 
-	const loader: Phaser.LoaderConfig = {
+	const loader: LoaderConfig = {
 	};
 
-	const config: Phaser.GameConfig = {
+	const render: RendererConfig = {
+		antialias: false,
+		pixelArt: true,
+		autoResize: false,
+		roundPixels: false,
+		transparent: false,
+		clearBeforeRender: false,
+		premultipliedAlpha: true,
+		preserveDrawingBuffer: false,
+		failIfMajorPerformanceCaveat: false,
+		powerPreference: 'default', // 'high-performance', 'low-power' or 'default'
+	}
+
+	const config: GameConfig = {
 		width: 800,
 		height: 600,
 		type: Phaser.CANVAS, // AUTO, CANVAS, WEBGL, HEADLESS
 		canvas,
 		disableContextMenu: true,
 		fps,
-		render: {
-			antialias: false,
-			pixelArt: true,
-			autoResize: false,
-			roundPixels: false,
-			transparent: false,
-			clearBeforeRender: false,
-			premultipliedAlpha: true,
-			preserveDrawingBuffer: false,
-			failIfMajorPerformanceCaveat: false,
-			powerPreference: 'default', // 'high-performance', 'low-power' or 'default'
-		},
+		render,
 		backgroundColor: 0x300000,
 		callbacks: {
 			preBoot: () => console.log('=== PRE BOOT'),
