1adbf2c02288c969687aec134bc4568d5b354b16
Paweł Kapalla <pkapalla@xesenix.pl>
2018-06-15 03:17:44 +0200

    XG-22 feat(fullscreen): simple setup

--- src/game-01/components/game-view/game-view.tsx
+++ src/game-01/components/game-view/game-view.tsx
@@ -2,25 +2,40 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 
 import PhaserViewComponent from 'game-01/components/phaser-view/phaser-view';
+import { toggleFullscreen } from 'lib/fullscreen/fullscreen';
 
 export interface IGameViewProps { }
 export interface IGameViewState {
+	fullscreen: boolean;
 	tab: 'configuration' | 'game';
 }
 
 class GameViewComponent extends React.Component<IGameViewProps, IGameViewState> {
+	private fullScreenContainer?: HTMLDivElement;
+
 	constructor(props) {
 		super(props);
-		this.state = { tab: 'game' };
+		this.state = {
+			fullscreen: false,
+			tab: 'game',
+		};
+	}
+
+	public componentDidUpdate() {
+		if (this.state.fullscreen && !!this.fullScreenContainer) {
+			console.log('fullScreenContainer', this.fullScreenContainer);
+			toggleFullscreen(this.fullScreenContainer);
+		}
 	}
 
 	public render(): any {
 		const { tab = 'game' } = this.state;
 
-		return (<div className="panel panel-primary">
+		return (<div className="panel panel-primary full-screen-container" ref={(el: HTMLDivElement) => this.fullScreenContainer = el}>
 			<ul>
 				{ tab === 'configuration' ? <li><a onClick={this.backHandle}>Back</a></li> : null }
 				{ tab === 'game' ? <li><a onClick={this.openConfigurationHandle}>Configuration</a></li> : null }
+				<li><a onClick={this.toggleFullScreen}>Fullscreen</a></li>
 			</ul>
 			{ tab === 'configuration' ? <div key={'configuration'}>CONFIGURATION</div> : null }
 			{ tab === 'game' ? <PhaserViewComponent keepInstanceOnRemove={true}/> : null }
@@ -36,6 +51,12 @@ class GameViewComponent extends React.Component<IGameViewProps, IGameViewState> 
 	private backHandle = (): void => {
 		this.setState({
 			tab: 'game',
+		});
+	}
+
+	private toggleFullScreen = (): void => {
+		this.setState({
+			fullscreen: !this.state.fullscreen,
 		});
 	}
 }
---
+++ src/lib/fullscreen/fullscreen.ts
@@ -0,0 +1,28 @@
+export const toggleFullscreen = (element = document.getElementsByTagName('body')) => {
+	if (
+		document.fullscreenElement ||
+		document.webkitFullscreenElement ||
+		document.mozFullScreenElement ||
+		document.msFullscreenElement
+	) {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		} else if (document.msExitFullscreen) {
+			document.msExitFullscreen();
+		}
+	} else {
+		if (element.requestFullscreen) {
+			element.requestFullscreen();
+		} else if (element.mozRequestFullScreen) {
+			element.mozRequestFullScreen();
+		} else if (element.webkitRequestFullscreen) {
+			element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		} else if (element.msRequestFullscreen) {
+			element.msRequestFullscreen();
+		}
+	}
+}
