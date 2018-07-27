e36384ad13c789ba0d1b508929d356f54955f53b
Paweł Kapalla <pkapalla@xesenix.pl>
2018-07-27 09:34:41 +0200

    XG-26 chore(package): clean up dependencies
    * there is something wrong with babel-polyfil it adds large core-js into bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 const { application, webpack } = require('xes-webpack-core');
@@ -5,12 +6,43 @@ const { application, webpack } = require('xes-webpack-core');
 const app = application.getEnvApp();
 const appWebpack = `./webpack.game-00.config.js`;
 
-const config = {
+const factoryConfig = {
 	useBabelrc: true,
-}
+};
 
 if (fs.existsSync(appWebpack)) {
-	module.exports = (env) => require(appWebpack)(webpack.webpackConfigFactory(config));
+	module.exports = (env) => require(appWebpack)(webpack.webpackConfigFactory(factoryConfig));
 } else {
-	module.exports = (env) => webpack.webpackConfigFactory(config);
+	module.exports = (env) => {
+		console.log(chalk.bold.yellow('Setting WEBPACK...'));
+		const config = webpack.webpackConfigFactory(factoryConfig);
+
+		config.output.filename = '[name].js';
+		config.output.chunkFilename = '[name].js';
+
+		// This cannot be used in testing environment
+		if (process.env.ENV !== 'test') {
+			config.optimization = {
+				...config.optimization,
+				splitChunks: {
+					automaticNameDelimiter: '.',
+					chunks: 'all',
+					name: true,
+					cacheGroups: {
+						default: {
+							minChunks: 2,
+							priority: -20,
+							reuseExistingChunk: true,
+						},
+						vendors: {
+							priority: -10,
+							test: /[\\/]node_modules[\\/]/,
+						},
+					},
+				},
+			};
+		}
+
+		return config;
+	};
 }
