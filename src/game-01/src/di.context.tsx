dd8e6b441ea5a87071774f296368d490571c1ef4
Paweł Kapalla <pkapalla@xesenix.pl>
2018-08-02 04:52:35 +0200

    XG-28 feat(di): inject helper component

--- src/game-01/src/di.context.tsx
+++ src/game-01/src/di.context.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import { Container } from 'inversify';
 
+// tslint:disable:max-classes-per-file
+
 export const DIContext = React.createContext<Container | null>(null);
 export function connectToDI<T>(Consumer) {
 	class DIConsumer extends React.Component<T, {}> {
@@ -11,4 +13,39 @@ export function connectToDI<T>(Consumer) {
 	}
 
 	return DIConsumer;
+}
+
+export function inject<T>(Consumer, select: { [key: string]: (value: any) => Promise<any> }) {
+	class Inject extends React.Component<T, {}> {
+		public componentDidMount() {
+			const { di } = this.props;
+
+			if (!!di) {
+				const keys = Object.keys(select);
+
+				Promise.all(
+					keys.map((key) => select[key](di.get(key))),
+				).then((values: any[]) => {
+					const state = values.reduce((result, value, index) => {
+						// TODO: finish mapping to properties
+						result[keys[index]] = value;
+						return result;
+					}, {});
+					this.setState(state);
+				});
+			}
+		}
+
+		public render() {
+			return (<DIContext.Consumer>{
+				(container: Container | null) => (
+					<Consumer
+						{...this.props}
+						{...this.state}
+					/>)
+			}</DIContext.Consumer>);
+		}
+	}
+
+	return connectToDI<T>(Inject);
 }
