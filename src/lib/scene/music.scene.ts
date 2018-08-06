ef1851571145dbc2c736c7ca2e235a2be35e9ae2
Paweł Kapalla <pkapalla@xesenix.pl>
2018-08-07 00:54:23 +0200

    XG-29 fix(sound): music loop
    * loop without creating multiple sound audio buffers after each loop

--- src/lib/scene/music.scene.ts
+++ src/lib/scene/music.scene.ts
@@ -1,14 +1,8 @@
 import * as Phaser from 'phaser';
-import { Store } from 'redux';
-
-import { IUIState } from 'game-01/src/ui/reducers';
-import { StorePlugin } from 'lib/phaser/store.plugin';
 
 export class MusicScene extends Phaser.Scene {
 	private soundtrack?: Phaser.Sound.BaseSound;
 	private label?: Phaser.GameObjects.Text;
-
-	private unsubscribe: any;
 
 	constructor() {
 		console.log('MusicScene:constructor');
@@ -28,18 +22,12 @@ export class MusicScene extends Phaser.Scene {
 		console.log('=== CREATE MUSIC');
 
 		this.soundtrack = this.sound.add('soundtrack');
-		this.soundtrack.play('', { loop: true });
+
+		this.soundtrack.play();
+		this.soundtrack.source.loop = true;
 
 		this.label = this.add.text(400, 300, '', { font: '64px Consolas', fill: '#ffffff' });
 		this.label.setOrigin(0.5, 0.5);
-
-		this.input.keyboard.on('keydown_P', () => {
-			if (!this.scene.isActive('music')) {
-				this.scene.resume('music');
-			} else {
-				this.scene.pause('music');
-			}
-		});
 	}
 
 	public destroy() {
@@ -51,6 +39,5 @@ export class MusicScene extends Phaser.Scene {
 
 	public update(time: number, delta: number) {
 		this.label.setText(`total time: ${(time / 1000).toFixed(0)}s\ndelta: ${delta.toFixed(2)}ms`);
-		// console.log('update', delta.toFixed(0));
 	}
 }
