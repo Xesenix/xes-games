2f557f2769cadc45e816c28ffcd91af1cebbf1b6
Paweł Kapalla <pkapalla@xesenix.pl>
2018-08-09 02:03:45 +0200

    XG-32 feat(ui): add sound related actions and reducers

--- src/game-01/src/ui/actions/index.ts
+++ src/game-01/src/ui/actions/index.ts
@@ -8,7 +8,19 @@ export interface ISetMuteAction extends Action {
 export const createSetMuteAction = (value: boolean): ISetMuteAction => ({
 	type: SET_MUTE,
 	value,
-})
+});
+
+export const SET_MUTE_MUSIC = 'UI_SET_MUTE_MUSIC';
+export const createSetMuteMusicAction = (value: boolean): ISetMuteAction => ({
+	type: SET_MUTE_MUSIC,
+	value,
+});
+
+export const SET_MUTE_SOUND = 'UI_SET_MUTE_SOUND';
+export const createSetMuteSoundAction = (value: boolean): ISetMuteAction => ({
+	type: SET_MUTE_SOUND,
+	value,
+});
 
 export const SET_PAUSE = 'UI_SET_PAUSE';
 export interface ISetPauseAction extends Action {
@@ -25,6 +37,18 @@ export interface ISetVolumeAction extends Action {
 }
 export const createSetVolumeAction = (value: number): ISetVolumeAction => ({
 	type: SET_VOLUME,
+	value,
+});
+
+export const SET_SOUND_VOLUME = 'UI_SET_SOUND_VOLUME';
+export const createSetSoundVolumeAction = (value: number): ISetVolumeAction => ({
+	type: SET_SOUND_VOLUME,
+	value,
+});
+
+export const SET_MUSIC_VOLUME = 'UI_SET_MUSIC_VOLUME';
+export const createSetMusicVolumeAction = (value: number): ISetVolumeAction => ({
+	type: SET_MUSIC_VOLUME,
 	value,
 });
 
--- src/game-01/src/ui/reducers/index.ts
+++ src/game-01/src/ui/reducers/index.ts
@@ -1,25 +1,37 @@
 import { Reducer } from 'redux';
 
 import {
-	SET_MUTE,
-	SET_PAUSE,
-	SET_VOLUME,
 	ISetMuteAction,
 	ISetPauseAction,
 	ISetVolumeAction,
+	SET_MUSIC_VOLUME,
+	SET_MUTE,
+	SET_MUTE_MUSIC,
+	SET_MUTE_SOUND,
+	SET_PAUSE,
+	SET_SOUND_VOLUME,
+	SET_VOLUME,
 	UIAction,
 } from '../actions/index';
 
 export interface IUIState {
 	mute: boolean;
+	muteMusic: boolean;
+	muteSound: boolean;
 	paused: boolean;
+	soundVolume: number;
+	musicVolume: number;
 	volume: number;
 }
 
 export const defaultUIState: IUIState = {
 	mute: false,
+	muteMusic: false,
+	muteSound: false,
 	paused: false,
+	soundVolume: 1.0,
 	volume: 0.5,
+	musicVolume: 1.0,
 };
 
 export const ui: Reducer<IUIState, UIAction> = (state: IUIState = defaultUIState, action: UIAction): IUIState => {
@@ -29,6 +41,20 @@ export const ui: Reducer<IUIState, UIAction> = (state: IUIState = defaultUIState
 			return {
 				...state,
 				mute: value,
+			};
+		}
+		case SET_MUTE_MUSIC: {
+			const { value } = action as ISetMuteAction;
+			return {
+				...state,
+				muteMusic: value,
+			};
+		}
+		case SET_MUTE_SOUND: {
+			const { value } = action as ISetMuteAction;
+			return {
+				...state,
+				muteSound: value,
 			};
 		}
 		case SET_PAUSE: {
@@ -43,6 +69,20 @@ export const ui: Reducer<IUIState, UIAction> = (state: IUIState = defaultUIState
 			return {
 				...state,
 				volume: value,
+			};
+		}
+		case SET_MUSIC_VOLUME: {
+			const { value } = action as ISetVolumeAction;
+			return {
+				...state,
+				musicVolume: value,
+			};
+		}
+		case SET_SOUND_VOLUME: {
+			const { value } = action as ISetVolumeAction;
+			return {
+				...state,
+				soundVolume: value,
 			};
 		}
 	}
