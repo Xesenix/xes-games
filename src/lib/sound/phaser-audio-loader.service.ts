315ab098b98b53499770387bac768b7b3c0a9207
Paweł Kapalla <pkapalla@xesenix.pl>
2018-08-28 02:30:50 +0200

    XG-38 fix(audio): phaser audio loader send load all when all files already loaded

--- src/lib/sound/phaser-audio-loader.service.ts
+++ src/lib/sound/phaser-audio-loader.service.ts
@@ -6,7 +6,7 @@ import { IAudioContextFactory, IAudioFile, IAudioFileLoader } from './interfaces
 @inject(['audio-repository', 'audio-context:factory'])
 export class PhaserAudioLoaderService implements IAudioFileLoader {
 	public loader?: Phaser.Loader.LoaderPlugin;
-	private loadQueue: { [key: string]: IAudioFile } = {};
+	private loadQueue: boolean[] = [];
 
 	constructor(
 		private repository: AudioBufferRepository,
@@ -19,15 +19,20 @@ export class PhaserAudioLoaderService implements IAudioFileLoader {
 	public setLoader(loader: Phaser.Loader.LoaderPlugin): void {
 		this.loader = loader;
 		// TODO: find better way to connect to phaser loader audio cache
-		(this.loader as any).cacheManager.audio.events.on('add', (cache: Phaser.Cache.BaseCache, key: string, data: AudioBuffer) => {
+		(this.loader as any).cacheManager.audio.events.on('add', (
+			cache: Phaser.Cache.BaseCache,
+			key: string,
+			data: AudioBuffer,
+		) => {
 			this.repository.add(key, data);
+			this.loadQueue[key] = false;
 		});
 	}
 
 	public add(key: string, url: string): void {
 		console.log('add', key, url);
 		if (!this.loadQueue[key]) {
-			this.loadQueue[key] = { key, url };
+			this.loadQueue[key] = true;
 		}
 		if (this.loader) {
 			// TODO: phaser has mismatched interface for configuring audioContext so we need cast second argument to any
@@ -46,10 +51,14 @@ export class PhaserAudioLoaderService implements IAudioFileLoader {
 
 	public loadAll(): Promise<void> {
 		return new Promise((resolve) => {
-			if (this.loader) {
-				this.loader.addListener('complete', () => {
-					resolve();
-				});
+			if (Object.values(this.loadQueue).some((loading) => loading)) {
+				if (this.loader) {
+					this.loader.addListener('complete', () => {
+						resolve();
+					});
+				}
+			} else {
+				resolve();
 			}
 		});
 	}
