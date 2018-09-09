829257a332cc98a3ea27751dd899809db4c83ab7
Paweł Kapalla <pkapalla@xesenix.pl>
2018-09-09 18:41:30 +0200

    XG-41 fix(tests): unify phaser setup for tests

--- webpack.game-01.config.js
+++ webpack.game-01.config.js
@@ -12,60 +12,53 @@ module.exports = (config) => {
 	console.log(chalk.bold.yellow('Setting WEBPACK for game-01...'));
 	config.module.rules.push(...webpack.loaders.shaderRulesFactory());
 
-	if (process.env.ENV === 'test') {
-		console.log(chalk.bold.yellow('Adding Phaser 3 environment setup...'));
-		config.plugins.push(new webpackBase.DefinePlugin({
-			// required by Phaser 3
-			'CANVAS_RENDERER': JSON.stringify(true),
-			'WEBGL_RENDERER': JSON.stringify(false),
-		}));
-	} else {
-		console.log(chalk.bold.yellow('Adding Phaser 3 environment setup...'));
-		config.plugins.push(new webpackBase.DefinePlugin({
-			// required by Phaser 3
-			'CANVAS_RENDERER': JSON.stringify(true),
-			'WEBGL_RENDERER': JSON.stringify(true),
-		}));
+	console.log(chalk.bold.yellow('Adding Phaser 3 environment setup...'));
+	config.plugins.push(new webpackBase.DefinePlugin({
+		// required by Phaser 3
+		'CANVAS_RENDERER': JSON.stringify(true),
+		'WEBGL_RENDERER': JSON.stringify(true),
+	}));
 
-		// VIS js css
-		console.log(chalk.bold.yellow('Adding loader for VIS JS assets...'));
-		config.module.rules.push({
-			test: /\.(svg|png|jpg|jpeg|gif)$/,
-			include: path.resolve('node_modules/vis/dist'),
-			use: {
-					loader: 'file-loader',
-					options: {
-							name: '[name].[ext]',
-							outputPath: 'assets/vis',
-					},
-			},
-		});
+	// VIS js css
+	console.log(chalk.bold.yellow('Adding loader for VIS JS assets...'));
+	config.module.rules.push({
+		test: /\.(svg|png|jpg|jpeg|gif)$/,
+		include: path.resolve('node_modules/vis/dist'),
+		use: {
+				loader: 'file-loader',
+				options: {
+						name: '[name].[ext]',
+						outputPath: 'assets/vis',
+				},
+		},
+	});
 
-		// VIS js has broken moment use implementation
-		// it needs global object just to not include useless locales
-		config.plugins.push(new webpackBase.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|pl)$/));
+	// VIS js has broken moment use implementation
+	// it needs global object just to not include useless locales
+	config.plugins.push(new webpackBase.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|pl)$/));
 
-		/*config.externals = {
-			...config.externals,
-			moment: 'moment',
-		};*/
+	/*config.externals = {
+		...config.externals,
+		moment: 'moment',
+	};*/
 
-		// config.devtool = 'cheap-module-source-map';
+	// config.devServer.disableHostCheck = true;
 
-		// all externals need to be manually added in template
-		// TODO: add them automatically
-		/*config.externals = {
-			...config.externals,
-			phaser: 'Phaser',
-		};
+	// config.devtool = 'cheap-module-source-map';
 
-		config.plugins.push(new CopyWebpackPlugin([
-			{
-				from: './node_modules/phaser/dist/phaser.min.js',
-				to: 'phaser.min.js',
-			},
-		]));*/
-	}
+	// all externals need to be manually added in template
+	// TODO: add them automatically
+	/*config.externals = {
+		...config.externals,
+		phaser: 'Phaser',
+	};
+
+	config.plugins.push(new CopyWebpackPlugin([
+		{
+			from: './node_modules/phaser/dist/phaser.min.js',
+			to: 'phaser.min.js',
+		},
+	]));*/
 
 	return config;
 };
